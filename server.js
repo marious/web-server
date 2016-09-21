c9e45e3312a07824975f08b48950f4cc5b8af14e
marious <mohammedraya410@gmail.com>
2016-09-21 20:12:21 +0200

    Refactor middleware

--- server.js
+++ server.js
@@ -1,25 +1,12 @@
 "use strict";
 const express = require("express");
 const path = require("path");
+const middleware = require('./middleware');
 const app = express();
 const PORT = 3000;
 const HOST = "127.0.0.1";
 const date = new Date();
 
-const middleware = {
-	requireAuthentication(req, res, next) {
-		console.log("prive route hit!");
-		next();
-	},
-
-	logger(req, res, next) {
-		console.log('Request: ' + new Date().toString() + ' ' + req.method + ' ' + req.originalUrl );
-		next();
-	}
-};
-
-// application middleware
-// app.use(middleware.requireAuthentication);
 
 app.use(middleware.logger);
 
