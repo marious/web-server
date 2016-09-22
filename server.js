5814de0134b7971a36562f67fd400eadd2a71c24
marious <mohammedraya410@gmail.com>
2016-09-22 17:46:29 +0200

    Updated added kanbanboard app

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 "use strict";
 const express = require("express");
 const path = require("path");
+const compression = require('compression');
 const middleware = require('./middleware');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const date = new Date();
 
+app.use(compression());
 
 app.use(middleware.logger);
 
